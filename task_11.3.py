f8c04485102b7da7cd93722fb58c1eb3fae6c3ad
Idel Yusupov <idel84@mail.ru>
2021-04-02 19:04:36 +0300

    homework_lesson_№_11

--- task_11.3.py
+++ task_11.3.py
@@ -3,29 +3,20 @@ class OwnError(Exception):
         self.txt = text
 
 
-class CheckType:
-    def __init__(self):
-        self.num_list = []
-
-    def check_value(self):
-        global num
-        while True:
-            try:
-                try:
-                    num = int(input('Введите число: '))
-                    question = input(f'"{num}" в списке. Для продолжения нажмите "Enter". Выход: "q"').lower()
-                    self.num_list.append(num)
-                    if question == 'q':
-                        print('Получился список: ', self.num_list)
-                        break
-                except ValueError:
-                    raise OwnError('Некоректное значение!')
-            except (OwnError, ValueError):
-                question = input(f'Вы ввели не число! Для продолжения нажмите "Enter". Выход: "q"').lower()
-                if question == 'q':
-                    print('Получился:', self.num_list)
-                    break
-
-
-item = CheckType()
-item.check_value()
+num_list = []
+while True:
+    try:
+        try:
+            num = int(input('\rВведите число: '))
+            question = input(f'"{num}" в списке. Для продолжения нажмите "Enter". Выход: "q"').lower()
+            num_list.append(num)
+            if question == 'q':
+                print('Получился список: ', num_list)
+                break
+        except ValueError:
+            raise OwnError('Некоректное значение!')
+    except (OwnError, ValueError):
+        question = input(f'Вы ввели не число! Для продолжения нажмите "Enter". Выход: "q"').lower()
+        if question == 'q':
+            print('Получился:', num_list)
+            break
